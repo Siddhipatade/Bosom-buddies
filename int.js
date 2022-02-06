46ed580b4df76ed183443229aba7c067de0e2b39
Siddhipatade <patadesiddhi20@gmail.com>
2022-02-06 14:09:41 +0530

    initial commit

---
+++ int.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ4riJ2Eomf0bu3_RHobl2CgOuBAt88JQ",
+    authDomain: "bosom-buddies-d3f09.firebaseapp.com",
+    projectId: "bosom-buddies-d3f09",
+    storageBucket: "bosom-buddies-d3f09.appspot.com",
+    messagingSenderId: "919429772765",
+    appId: "1:919429772765:web:2dac7db50c4ccf5f247234",
+    measurementId: "G-57MXENPEVS"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
---
+++ login.js
@@ -0,0 +1,49 @@
+
+
+firebase.auth().onAuthStateChange(function(user){
+  if(user){
+
+    document.getElementById("user_div").style.display="block";
+    document.getElementById("login_div").styles.display="none";
+
+    var user = firebase.auth().currentUser;
+
+    if (user !=null){
+      var email_id = user.email;
+      var uname = user.displayName;
+
+
+      document.getElementById("user_name_from_database").value = uname;
+      document.getElementById("user_email_from_database").value=email_id;
+      document.getElementById("profilepic").src = dp_url;
+    }
+
+  }
+  else{
+    document.getElementById("user_div").style.display="none";
+    document.getElementById("login_div").style.display="block";
+  }
+})
+
+
+function login(){
+  var email_address = document.getElementById("email_field").value;
+  var password = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword("email_address,password").catch(function(Error){
+    console.log(error);
+
+    if (username === "user" && password === "web_dev") {
+      alert("You have successfully logged in.");
+      location.reload();
+  } else {
+      loginErrorMsg.style.opacity = 1;
+  }
+  });
+  
+}
+
+function logout(){
+  firebase.auth().signOut();
+
+}
