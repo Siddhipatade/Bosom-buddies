1e4b3f25ea7fb96a450fdaabca6a30e623739f97
Siddhipatade <patadesiddhi20@gmail.com>
2022-02-09 17:13:19 +0530

    Bosom Buddies

--- login.js
+++ login.js
@@ -1,25 +1,25 @@
-firebase.auth().onAuthStateChanged(function(user){
-  if(user){
+//firebase.auth().onAuthStateChanged(function(user){
+  //if(user){
 
-    document.getElementById("user_div").style.display="block";
-    document.getElementById("login_div").style.display="none";
+    //document.getElementById("user_div").style.display="block";
+    //document.getElementById("login_div").style.display="none";
 
-    var user = firebase.auth().currentUser;
+    //var user = firebase.auth().currentUser;
 
-    if (user !=null){
-      var email_id = user.email;
-      var uname = user.displayName;
+    //if (user !=null){
+      //var email_id = user.email;
+      //var uname = user.displayName;
 
 
-      document.getElementById("user_name_from_database").value = uname;
-      document.getElementById("user_email_from_database").value= email_id;
+      //document.getElementById("user_name_from_database").value = uname;
+      //document.getElementById("user_email_from_database").value= email_id;
 
-  }
-  else{
+ // }
+  //else{
     // document.getElementById("user_div").style.display="none";
     // document.getElementById("login_div").style.display="block";
-  }
-})
+ // }
+//})
 
 
 function login(){
@@ -29,7 +29,7 @@ function login(){
     console.log(error);
     if (username === "user" && password === "Arf@78dd") {
       alert("You have successfully logged in.");
-      location.href="home.html";
+      location.href="index.html";
   } else {
       loginErrorMsg.style.opacity = 1;
   }
@@ -43,7 +43,7 @@ function signup(){
     console.log(error);
     if (username === "user" && password === "Arf@78dd") {
       alert("You have successfully logged in.");
-      location.href="home.html";
+      location.href="index.html";
   } else {
       loginErrorMsg.style.opacity = 1;
   }
